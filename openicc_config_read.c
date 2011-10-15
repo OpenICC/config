04e1176461c346977099b8f9f12f046a35f6b273
Kai-Uwe Behrmann <ku.b@gmx.de>
2011-10-15 10:10:47 +0200

    * [exmpl]: add more comments and a new example snippet
    The new example code shows how to read only a selected device class and
    write that into a separate OpenICC JSON DB file stream. For simplicity we use
    just stdout.

--- openicc_config_read.c
+++ openicc_config_read.c
@@ -11,18 +11,20 @@ int main(int argc, char ** argv)
   FILE * fp = fopen(file_name,"rb");
   char * text = 0;
   size_t size = 0;
-  int error = 0;
   char            ** keys = 0;
   char            ** values = 0;
-  int i,j, n = 0, devices_n;
+  int i,j, n = 0, devices_n, flags;
   char * json, * device_class;
+  const char * devices_filter[] = {OPENICC_DEVICE_CAMERA,NULL};
 
+  /* show help text */
   if(argc == 1)
   {
     fprintf( stderr, "Usage: %s openicc.json\n\n", argv[0] );
     return 0;
   }
 
+  /* read JSON input file */
   if(fp)
   { 
     fseek(fp,0L,SEEK_END);
@@ -38,13 +40,14 @@ int main(int argc, char ** argv)
   }
  
 
+  /* parse JSON */
   configs = openiccConfigs_FromMem( text );
   openiccConfigs_SetInfo ( configs, file_name );
   devices_n = openiccConfigs_Count(configs, NULL);
   fprintf(stderr, "Found %d devices.\n", devices_n );
 
   
-
+  /* print all found key/value pairs */
   for(i = 0; i < devices_n; ++i)
   {
     const char * d = openiccConfigs_DeviceGet( configs, NULL, i,
@@ -64,11 +67,35 @@ int main(int argc, char ** argv)
     free(keys); free(values);
   }
 
-  json = openiccConfigs_DeviceGetJSON ( configs, NULL, 0, 0, malloc );
+  /* get a single JSON device */
+  i = 1; /* select the second one, we start counting from zero */
+  json = openiccConfigs_DeviceGetJSON ( configs, NULL, i, 0, malloc );
   config = openiccConfigs_FromMem( json );
   device_class = openiccConfigs_DeviceClassGet( config, malloc );
-  fprintf( stderr, "\ndevice class: \"%s\"\n", device_class);
+  fprintf( stderr, "\ndevice class[%d]: \"%s\"\n", i, device_class);
   printf( "%s\n", json );
+  free(json);
+
+
+  /* we want a single device class DB for lets say cameras */
+  devices_n = openiccConfigs_Count(configs, devices_filter);
+  fprintf(stderr, "Found %d %s devices.\n", devices_n, devices_filter[0] );
+  for(i = 0; i < devices_n; ++i)
+  {
+    flags = 0;
+    if(i != 0) /* not the first */
+      flags |= OPENICC_CONFIGS_SKIP_HEADER;
+    if(i != devices_n - 1) /* not the last */
+      flags |= OPENICC_CONFIGS_SKIP_FOOTER;
+    else /* end the current JSON array field and open the next one */
+      printf("            },\n            {\n");
+
+    json = openiccConfigs_DeviceGetJSON( configs, devices_filter, i,
+                                         flags, malloc );
+
+    printf( "%s\n", json );
+    free(json);
+  }
 
   openiccConfigs_Release( &configs );
 
