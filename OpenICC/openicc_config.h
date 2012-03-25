4d50843d111d6f0c61486217e1c1742fb489d935
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-03-25 20:09:15 +0200

    * [core]: add OpenICC paths
    They follow the OpenIccDirectoryProposal.

--- OpenICC/openicc_config.h
+++ OpenICC/openicc_config.h
@@ -26,6 +26,11 @@ extern "C" {
  *  @{
  */
 
+#define OPENICC_HOME_COLOR_PATH "~/.config/color/settings"
+#define OPENICC_SYSTEM_COLOR_PATH "/etc/xdg/color/settings"
+#define OPENICC_SETTINGS_DB "openicc-defaults.json"
+#define OPENICC_DEVICES_DB "openicc-devices.json"
+
 #define OPENICC_BASE_PATH "org/freedesktop/openicc/device"
 #define OPENICC_DEVICE_MONITOR "monitor"
 #define OPENICC_DEVICE_SCANNER "scanner"
