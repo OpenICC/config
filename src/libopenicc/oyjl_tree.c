f2fb689b4395e70a5bb6655b194037bf4b1b9eb4
Kai-Uwe Behrmann <ku.b@gmx.de>
2016-10-19 11:45:56 +0200

    * [core]: skip code duplicates with libyajl in oyjl

--- src/libopenicc/oyjl_tree.c
+++ src/libopenicc/oyjl_tree.c
@@ -28,501 +28,10 @@
 #include <yajl/yajl_version.h>
 #endif
 #include "oyjl_tree.h"
-#define YA_FREE(afs, ptr) (afs)->free((afs)->ctx, (ptr))
 
 #if defined(_MSC_VER) 
 #define snprintf sprintf_s
 #endif
-
-#define STATUS_CONTINUE 1
-#define STATUS_ABORT    0
-
-struct stack_elem_s;
-typedef struct stack_elem_s stack_elem_t;
-struct stack_elem_s
-{
-    char * key;
-    oyjl_val value;
-    stack_elem_t *next;
-};
-
-struct context_s
-{
-    stack_elem_t *stack;
-    oyjl_val root;
-    char *errbuf;
-    size_t errbuf_size;
-};
-typedef struct context_s context_t;
-
-#define RETURN_ERROR(ctx,retval,...) {                                  \
-        if ((ctx)->errbuf != NULL)                                      \
-            snprintf ((ctx)->errbuf, (ctx)->errbuf_size, __VA_ARGS__);  \
-        return (retval);                                                \
-    }
-
-static oyjl_val value_alloc (oyjl_type type)
-{
-    oyjl_val v;
-
-    v = malloc (sizeof (*v));
-    if (v == NULL) return (NULL);
-    memset (v, 0, sizeof (*v));
-    v->type = type;
-
-    return (v);
-}
-
-static void oyjl_object_free (oyjl_val v)
-{
-    size_t i;
-
-    if (!OYJL_IS_OBJECT(v)) return;
-
-    for (i = 0; i < v->u.object.len; i++)
-    {
-        free((char *) v->u.object.keys[i]);
-        v->u.object.keys[i] = NULL;
-        oyjl_tree_free (v->u.object.values[i]);
-        v->u.object.values[i] = NULL;
-    }
-
-    free((void*) v->u.object.keys);
-    free(v->u.object.values);
-    free(v);
-}
-
-static void oyjl_array_free (oyjl_val v)
-{
-    size_t i;
-
-    if (!OYJL_IS_ARRAY(v)) return;
-
-    for (i = 0; i < v->u.array.len; i++)
-    {
-        oyjl_tree_free (v->u.array.values[i]);
-        v->u.array.values[i] = NULL;
-    }
-
-    free(v->u.array.values);
-    free(v);
-}
-
-/*
- * Parsing nested objects and arrays is implemented using a stack. When a new
- * object or array starts (a curly or a square opening bracket is read), an
- * appropriate value is pushed on the stack. When the end of the object is
- * reached (an appropriate closing bracket has been read), the value is popped
- * off the stack and added to the enclosing object using "context_add_value".
- */
-static int context_push(context_t *ctx, oyjl_val v)
-{
-    stack_elem_t *stack;
-
-    stack = malloc (sizeof (*stack));
-    if (stack == NULL)
-        RETURN_ERROR (ctx, ENOMEM, "Out of memory");
-    memset (stack, 0, sizeof (*stack));
-
-    assert ((ctx->stack == NULL)
-            || OYJL_IS_OBJECT (v)
-            || OYJL_IS_ARRAY (v));
-
-    stack->value = v;
-    stack->next = ctx->stack;
-    ctx->stack = stack;
-
-    return (0);
-}
-
-static oyjl_val context_pop(context_t *ctx)
-{
-    stack_elem_t *stack;
-    oyjl_val v;
-
-    if (ctx->stack == NULL)
-        RETURN_ERROR (ctx, NULL, "context_pop: "
-                      "Bottom of stack reached prematurely");
-
-    stack = ctx->stack;
-    ctx->stack = stack->next;
-
-    v = stack->value;
-
-    free (stack);
-
-    return (v);
-}
-
-static int object_add_keyval(context_t *ctx,
-                             oyjl_val obj, char *key, oyjl_val value)
-{
-    const char **tmpk;
-    oyjl_val *tmpv;
-
-    /* We're checking for NULL in "context_add_value" or its callers. */
-    assert (ctx != NULL);
-    assert (obj != NULL);
-    assert (key != NULL);
-    assert (value != NULL);
-
-    /* We're assuring that "obj" is an object in "context_add_value". */
-    assert(OYJL_IS_OBJECT(obj));
-
-    tmpk = realloc((void *) obj->u.object.keys, sizeof(*(obj->u.object.keys)) * (obj->u.object.len + 1));
-    if (tmpk == NULL)
-        RETURN_ERROR(ctx, ENOMEM, "Out of memory");
-    obj->u.object.keys = tmpk;
-
-    tmpv = realloc(obj->u.object.values, sizeof (*obj->u.object.values) * (obj->u.object.len + 1));
-    if (tmpv == NULL)
-        RETURN_ERROR(ctx, ENOMEM, "Out of memory");
-    obj->u.object.values = tmpv;
-
-    obj->u.object.keys[obj->u.object.len] = key;
-    obj->u.object.values[obj->u.object.len] = value;
-    obj->u.object.len++;
-
-    return (0);
-}
-
-static int array_add_value (context_t *ctx,
-                            oyjl_val array, oyjl_val value)
-{
-    oyjl_val *tmp;
-
-    /* We're checking for NULL pointers in "context_add_value" or its
-     * callers. */
-    assert (ctx != NULL);
-    assert (array != NULL);
-    assert (value != NULL);
-
-    /* "context_add_value" will only call us with array values. */
-    assert(OYJL_IS_ARRAY(array));
-    
-    tmp = realloc(array->u.array.values,
-                  sizeof(*(array->u.array.values)) * (array->u.array.len + 1));
-    if (tmp == NULL)
-        RETURN_ERROR(ctx, ENOMEM, "Out of memory");
-    array->u.array.values = tmp;
-    array->u.array.values[array->u.array.len] = value;
-    array->u.array.len++;
-
-    return 0;
-}
-
-/*
- * Add a value to the value on top of the stack or the "root" member in the
- * context if the end of the parsing process is reached.
- */
-static int context_add_value (context_t *ctx, oyjl_val v)
-{
-    /* We're checking for NULL values in all the calling functions. */
-    assert (ctx != NULL);
-    assert (v != NULL);
-
-    /*
-     * There are three valid states in which this function may be called:
-     *   - There is no value on the stack => This is the only value. This is the
-     *     last step done when parsing a document. We assign the value to the
-     *     "root" member and return.
-     *   - The value on the stack is an object. In this case store the key on the
-     *     stack or, if the key has already been read, add key and value to the
-     *     object.
-     *   - The value on the stack is an array. In this case simply add the value
-     *     and return.
-     */
-    if (ctx->stack == NULL)
-    {
-        assert (ctx->root == NULL);
-        ctx->root = v;
-        return (0);
-    }
-    else if (OYJL_IS_OBJECT (ctx->stack->value))
-    {
-        if (ctx->stack->key == NULL)
-        {
-            if (!OYJL_IS_STRING (v))
-                RETURN_ERROR (ctx, EINVAL, "context_add_value: "
-                              "Object key is not a string (%#04x)",
-                              v->type);
-
-            ctx->stack->key = v->u.string;
-            v->u.string = NULL;
-            free(v);
-            return (0);
-        }
-        else /* if (ctx->key != NULL) */
-        {
-            char * key;
-
-            key = ctx->stack->key;
-            ctx->stack->key = NULL;
-            return (object_add_keyval (ctx, ctx->stack->value, key, v));
-        }
-    }
-    else if (OYJL_IS_ARRAY (ctx->stack->value))
-    {
-        return (array_add_value (ctx, ctx->stack->value, v));
-    }
-    else
-    {
-        RETURN_ERROR (ctx, EINVAL, "context_add_value: Cannot add value to "
-                      "a value of type %#04x (not a composite type)",
-                      ctx->stack->value->type);
-    }
-}
-
-static int handle_string (void *ctx,
-                          const unsigned char *string, unsigned int string_length)
-{
-    oyjl_val v;
-
-    v = value_alloc (oyjl_t_string);
-    if (v == NULL)
-        RETURN_ERROR ((context_t *) ctx, STATUS_ABORT, "Out of memory");
-
-    v->u.string = malloc (string_length + 1);
-    if (v->u.string == NULL)
-    {
-        free (v);
-        RETURN_ERROR ((context_t *) ctx, STATUS_ABORT, "Out of memory");
-    }
-    memcpy(v->u.string, string, string_length);
-    v->u.string[string_length] = 0;
-
-    return ((context_add_value (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-#if (YAJL_VERSION) > 20000
-static int handle_number (void *ctx, const char *string, size_t string_length)
-#else
-static int handle_number (void *ctx, const char *string, unsigned int string_length)
-#endif
-{
-    oyjl_val v;
-    char *endptr;
-
-    v = value_alloc(oyjl_t_number);
-    if (v == NULL)
-        RETURN_ERROR((context_t *) ctx, STATUS_ABORT, "Out of memory");
-
-    v->u.number.r = malloc(string_length + 1);
-    if (v->u.number.r == NULL)
-    {
-        free(v);
-        RETURN_ERROR((context_t *) ctx, STATUS_ABORT, "Out of memory");
-    }
-    memcpy(v->u.number.r, string, string_length);
-    v->u.number.r[string_length] = 0;
-
-    v->u.number.flags = 0;
-
-    errno = 0;
-    v->u.number.i = strtol(v->u.number.r, 0, 10);
-    if (errno == 0)
-        v->u.number.flags |= OYJL_NUMBER_INT_VALID;
-
-    endptr = NULL;
-    errno = 0;
-    v->u.number.d = strtod(v->u.number.r, &endptr);
-    if ((errno == 0) && (endptr != NULL) && (*endptr == 0))
-        v->u.number.flags |= OYJL_NUMBER_DOUBLE_VALID;
-
-    return ((context_add_value(ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-static int handle_start_map (void *ctx)
-{
-    oyjl_val v;
-
-    v = value_alloc(oyjl_t_object);
-    if (v == NULL)
-        RETURN_ERROR ((context_t *) ctx, STATUS_ABORT, "Out of memory");
-
-    v->u.object.keys = NULL;
-    v->u.object.values = NULL;
-    v->u.object.len = 0;
-
-    return ((context_push (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-static int handle_end_map (void *ctx)
-{
-    oyjl_val v;
-
-    v = context_pop (ctx);
-    if (v == NULL)
-        return (STATUS_ABORT);
-
-    return ((context_add_value (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-static int handle_start_array (void *ctx)
-{
-    oyjl_val v;
-
-    v = value_alloc(oyjl_t_array);
-    if (v == NULL)
-        RETURN_ERROR ((context_t *) ctx, STATUS_ABORT, "Out of memory");
-
-    v->u.array.values = NULL;
-    v->u.array.len = 0;
-
-    return ((context_push (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-static int handle_end_array (void *ctx)
-{
-    oyjl_val v;
-
-    v = context_pop (ctx);
-    if (v == NULL)
-        return (STATUS_ABORT);
-
-    return ((context_add_value (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-static int handle_boolean (void *ctx, int boolean_value)
-{
-    oyjl_val v;
-
-    v = value_alloc (boolean_value ? oyjl_t_true : oyjl_t_false);
-    if (v == NULL)
-        RETURN_ERROR ((context_t *) ctx, STATUS_ABORT, "Out of memory");
-
-    return ((context_add_value (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-static int handle_null (void *ctx)
-{
-    oyjl_val v;
-
-    v = value_alloc (oyjl_t_null);
-    if (v == NULL)
-        RETURN_ERROR ((context_t *) ctx, STATUS_ABORT, "Out of memory");
-
-    return ((context_add_value (ctx, v) == 0) ? STATUS_CONTINUE : STATUS_ABORT);
-}
-
-/*
- * Public functions
- */
-oyjl_val oyjl_tree_parse (const char *input,
-                          char *error_buffer, size_t error_buffer_size)
-{
-#if (YAJL_VERSION) > 20000
-static yajl_callbacks oyjl_tree_callbacks = {
-  handle_null,
-  handle_boolean,
-  NULL, //handle_integer,
-  NULL, //handle_double,
-  handle_number,
-  (int(*)(void*,const unsigned char*,size_t))handle_string,
-  handle_start_map,
-  (int(*)(void*,const unsigned char*,size_t))handle_string,
-  handle_end_map,
-  handle_start_array,
-  handle_end_array
-};
-#else
-static yajl_callbacks oyjl_tree_callbacks = {
-  handle_null,
-  handle_boolean,
-  NULL, //handle_integer,
-  NULL, //handle_double,
-  handle_number,
-  handle_string,
-  handle_start_map,
-  handle_string,
-  handle_end_map,
-  handle_start_array,
-  handle_end_array
-};
-#endif
-#if 0
-    static const yajl_callbacks callbacks =
-        {
-            /* null        = */ handle_null,
-            /* boolean     = */ handle_boolean,
-            /* integer     = */ NULL,
-            /* double      = */ NULL,
-            /* number      = */ handle_number,
-            /* string      = */ handle_string,
-            /* start map   = */ handle_start_map,
-            /* map key     = */ handle_string,
-            /* end map     = */ handle_end_map,
-            /* start array = */ handle_start_array,
-            /* end array   = */ handle_end_array
-        };
-#endif
-    yajl_handle handle;
-#if YAJL_VERSION < 20000
-    yajl_parser_config yconfig = { 1, 1 };
-#endif
-    yajl_status status;
-    char * internal_err_str;
-	context_t ctx = { NULL, NULL, NULL, 0 };
-
-	ctx.errbuf = error_buffer;
-	ctx.errbuf_size = error_buffer_size;
-
-    if (error_buffer != NULL)
-        memset (error_buffer, 0, error_buffer_size);
-
-    handle = yajl_alloc( &oyjl_tree_callbacks,
-#if YAJL_VERSION < 20000
-                                                &yconfig,
-#endif
-                                                          NULL, (void*)&ctx);
-#if YAJL_VERSION > 19999
-    yajl_config(handle, yajl_allow_comments, 1);
-#endif
-
-    status = yajl_parse(handle,
-                        (unsigned char *) input,
-                        strlen (input));
-#if YAJL_VERSION > 19999
-    status = yajl_complete_parse (handle);
-#endif
-    if (status != yajl_status_ok) {
-        if (error_buffer != NULL && error_buffer_size > 0) {
-               internal_err_str = (char *) yajl_get_error(handle, 1,
-                     (const unsigned char *) input,
-                     strlen(input));
-             snprintf(error_buffer, error_buffer_size, "%s", internal_err_str);
-             yajl_free_error( handle, (unsigned char*)internal_err_str );
-             internal_err_str = 0;
-        }
-        yajl_free (handle);
-        return NULL;
-    }
-
-    yajl_free (handle);
-    return (ctx.root);
-}
-
-oyjl_val oyjl_tree_get(oyjl_val n, const char ** path, oyjl_type type)
-{
-    if (!path) return NULL;
-    while (n && *path) {
-        size_t i;
-        size_t len;
-
-        if (n->type != oyjl_t_object) return NULL;
-        len = n->u.object.len;
-        for (i = 0; i < len; i++) {
-            if (!strcmp(*path, n->u.object.keys[i])) {
-                n = n->u.object.values[i];
-                break;
-            }
-        }
-        if (i == len) return NULL;
-        path++;
-    }
-    if (n && type != oyjl_t_any && type != n->type) n = NULL;
-    return n;
-}
 
 char *             oyjl_string_copy  ( char              * string,
                                        void*            (* alloc)(size_t size))
@@ -577,29 +86,29 @@ int                   oyjl_string_add( char             ** string,
   return 0;
 }
 
-char * oyjl_value_text (oyjl_val v, void*(*alloc)(size_t size))
+char * oyjl_value_text (yajl_val v, void*(*alloc)(size_t size))
 {
   char * t = 0, * text = 0;
 
   if(v)
   switch(v->type)
   {
-    case oyjl_t_null:
+    case yajl_t_null:
          break;
-    case oyjl_t_number:
-         if(v->u.number.flags & OYJL_NUMBER_DOUBLE_VALID)
+    case yajl_t_number:
+         if(v->u.number.flags & YAJL_NUMBER_DOUBLE_VALID)
            oyjl_string_add (&t, "%g", v->u.number.d);
          else
            oyjl_string_add (&t, "%ld", v->u.number.i);
          break;
-    case oyjl_t_true:
+    case yajl_t_true:
          oyjl_string_add (&t, "1"); break;
-    case oyjl_t_false:
+    case yajl_t_false:
          oyjl_string_add (&t, "0"); break;
-    case oyjl_t_string:
+    case yajl_t_string:
          oyjl_string_add (&t, "%s", v->u.string); break;
-    case oyjl_t_array:
-    case oyjl_t_object:
+    case yajl_t_array:
+    case yajl_t_object:
          break;
     default:
          fprintf( stderr, "unknown type: %d\n", v->type );
@@ -615,28 +124,28 @@ char * oyjl_value_text (oyjl_val v, void*(*alloc)(size_t size))
   return text;
 }
 
-void oyjl_tree_to_json (oyjl_val v, int * level, char ** json)
+void oyjl_tree_to_json (yajl_val v, int * level, char ** json)
 {
   int n = *level;
 
   if(v)
   switch(v->type)
   {
-    case oyjl_t_null:
+    case yajl_t_null:
          break;
-    case oyjl_t_number:
-         if(v->u.number.flags & OYJL_NUMBER_DOUBLE_VALID)
+    case yajl_t_number:
+         if(v->u.number.flags & YAJL_NUMBER_DOUBLE_VALID)
            oyjl_string_add (json, "%g", v->u.number.d);
          else
            oyjl_string_add (json, "%ld", v->u.number.i);
          break;
-    case oyjl_t_true:
+    case yajl_t_true:
          oyjl_string_add (json, "1"); break;
-    case oyjl_t_false:
+    case yajl_t_false:
          oyjl_string_add (json, "0"); break;
-    case oyjl_t_string:
+    case yajl_t_string:
          oyjl_string_add (json, "\"%s\"", v->u.string); break;
-    case oyjl_t_array:
+    case yajl_t_array:
          {
            int i,
                count = v->u.array.len;
@@ -657,7 +166,7 @@ void oyjl_tree_to_json (oyjl_val v, int * level, char ** json)
 
            oyjl_string_add( json, "]");
          } break;
-    case oyjl_t_object:
+    case yajl_t_object:
          {
            int i,
                count = v->u.object.len;
@@ -691,30 +200,30 @@ void oyjl_tree_to_json (oyjl_val v, int * level, char ** json)
   return;
 }
 
-int            oyjl_value_count      ( oyjl_val            v )
+int            oyjl_value_count      ( yajl_val            v )
 {
   int count = 0;
 
   if(!v)
     return count;
 
-  if(v->type == oyjl_t_object)
+  if(v->type == yajl_t_object)
     count = v->u.object.len;
-  else if(v->type == oyjl_t_array)
+  else if(v->type == yajl_t_array)
     count = v->u.array.len;
 
   return count;
 }
 
-oyjl_val       oyjl_value_pos_get    ( oyjl_val            v,
+yajl_val       oyjl_value_pos_get    ( yajl_val            v,
                                        int                 pos )
 {
   if(!v)
     return NULL;
 
-  if(v->type == oyjl_t_object)
+  if(v->type == yajl_t_object)
     return v->u.object.values[pos];
-  else if(v->type == oyjl_t_array)
+  else if(v->type == yajl_t_array)
     return v->u.array.values[pos];
 
   return NULL;
@@ -768,10 +277,10 @@ char **        oyjl_string_split     ( const char        * text,
   return list;
 }
 
-oyjl_val   oyjl_tree_get_value       ( oyjl_val            v,
+yajl_val   oyjl_tree_get_value       ( yajl_val            v,
                                        const char        * xpath )
 {
-  oyjl_val level = 0;
+  yajl_val level = 0;
   int n = 0, i, found = 0;
   char ** list = oyjl_string_split(xpath, &n),
         * ttmp = 0;
@@ -857,11 +366,11 @@ oyjl_val   oyjl_tree_get_value       ( oyjl_val            v,
  *  @since   2011/09/24 (Oyranos: 0.3.3)
  *  @date    2013/02/24
  */
-oyjl_val   oyjl_tree_get_valuef      ( oyjl_val            v,
+yajl_val   oyjl_tree_get_valuef      ( yajl_val            v,
                                        const char        * format,
                                                            ... )
 {
-  oyjl_val value = 0;
+  yajl_val value = 0;
 
   char * text = 0;
   va_list list;
@@ -897,30 +406,3 @@ oyjl_val   oyjl_tree_get_valuef      ( oyjl_val            v,
 }
 
 
-void oyjl_tree_free (oyjl_val v)
-{
-    if (v == NULL) return;
-
-    if (OYJL_IS_STRING(v))
-    {
-        free(v->u.string);
-        free(v);
-    }
-    else if (OYJL_IS_NUMBER(v))
-    {
-        free(v->u.number.r);
-        free(v);
-    }
-    else if (OYJL_GET_OBJECT(v))
-    {
-        oyjl_object_free(v);
-    }
-    else if (OYJL_GET_ARRAY(v))
-    {
-        oyjl_array_free(v);
-    }
-    else /* if (oyjl_t_true or oyjl_t_false or oyjl_t_null) */
-    {
-        free(v);
-    }
-}
--- src/libopenicc/oyjl_tree.h
+++ src/libopenicc/oyjl_tree.h
@@ -35,6 +35,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <yajl/yajl_parse.h>
+#include <yajl/yajl_tree.h>
 #ifndef OYJL_API
 #define OYJL_API
 #endif
@@ -43,159 +44,18 @@
 extern "C" {
 #endif
 
-/** possible data types that a oyjl_val_s can hold */
-typedef enum {
-    oyjl_t_string = 1,
-    oyjl_t_number = 2,
-    oyjl_t_object = 3,
-    oyjl_t_array = 4,
-    oyjl_t_true = 5,
-    oyjl_t_false = 6,
-    oyjl_t_null = 7,
-    /** The any type isn't valid for oyjl_val_s.type, but can be
-     *  used as an argument to routines like oyjl_tree_get().
-     */
-    oyjl_t_any = 8
-} oyjl_type;
-
-#define OYJL_NUMBER_INT_VALID    0x01
-#define OYJL_NUMBER_DOUBLE_VALID 0x02
-
-/** A pointer to a node in the parse tree */
-typedef struct oyjl_val_s * oyjl_val;
-
-/**
- * A JSON value representation capable of holding one of the seven
- * types above. For "string", "number", "object", and "array"
- * additional data is available in the union.  The "OYJL_IS_*"
- * and "OYJL_GET_*" macros below allow type checking and convenient
- * value extraction.
- */
-struct oyjl_val_s
-{
-    /** Type of the value contained. Use the "OYJL_IS_*" macors to check for a
-     * specific type. */
-    oyjl_type type;
-    /** Type-specific data. You may use the "OYJL_GET_*" macros to access these
-     * members. */
-    union
-    {
-        char * string;
-        struct {
-            long long i; /*< integer value, if representable. */
-            double  d;   /*< double value, if representable. */
-            /** Signals whether the \em i and \em d members are
-             * valid. See \c OYJL_NUMBER_INT_VALID and
-             * \c OYJL_NUMBER_DOUBLE_VALID. */
-            char   *r;   /*< unparsed number in string form. */
-            unsigned int flags;
-        } number;
-        struct {
-            const char **keys; /*< Array of keys */
-            oyjl_val *values; /*< Array of values. */
-            size_t len; /*< Number of key-value-pairs. */
-        } object;
-        struct {
-            oyjl_val *values; /*< Array of elements. */
-            size_t len; /*< Number of elements. */
-        } array;
-    } u;
-};
-
-/**
- * Parse a string.
- *
- * Parses an null-terminated string containing JSON data and returns a pointer
- * to the top-level value (root of the parse tree).
- *
- * \param input              Pointer to a null-terminated utf8 string containing
- *                           JSON data.
- * \param error_buffer       Pointer to a buffer in which an error message will
- *                           be stored if \em oyjl_tree_parse fails, or
- *                           \c NULL. The buffer will be initialized before
- *                           parsing, so its content will be destroyed even if
- *                           \em oyjl_tree_parse succeeds.
- * \param error_buffer_size  Size of the memory area pointed to by
- *                           \em error_buffer_size. If \em error_buffer_size is
- *                           \c NULL, this argument is ignored.
- *
- * \returns Pointer to the top-level value or \c NULL on error. The memory
- * pointed to must be freed using \em oyjl_tree_free. In case of an error, a
- * null terminated message describing the error in more detail is stored in
- * \em error_buffer if it is not \c NULL.
- */
-OYJL_API oyjl_val oyjl_tree_parse (const char *input,
-                                   char *error_buffer, size_t error_buffer_size);
-
-/**
- * Free a parse tree returned by "oyjl_tree_parse".
- *
- * \param v Pointer to a JSON value returned by "oyjl_tree_parse". Passing NULL
- * is valid and results in a no-op.
- */
-OYJL_API void oyjl_tree_free (oyjl_val v);
-
-/**
- * Access a nested value inside a tree.
- *
- * \param parent the node under which you'd like to extract values.
- * \param path A null terminated array of strings, each the name of an object key
- * \param type the oyjl_type of the object you seek, or oyjl_t_any if any will do.
- *
- * \returns a pointer to the found value, or NULL if we came up empty.
- * 
- * Future Ideas:  it'd be nice to move path to a string and implement support for
- * a teeny tiny micro language here, so you can extract array elements, do things
- * like .first and .last, even .length.  Inspiration from JSONPath and css selectors?
- * No it wouldn't be fast, but that's not what this API is about.
- */
-OYJL_API oyjl_val oyjl_tree_get(oyjl_val parent, const char ** path, oyjl_type type);
-
-/* Various convenience macros to check the type of a `oyjl_val` */
-#define OYJL_IS_STRING(v) (((v) != NULL) && ((v)->type == oyjl_t_string))
-#define OYJL_IS_NUMBER(v) (((v) != NULL) && ((v)->type == oyjl_t_number))
-#define OYJL_IS_INTEGER(v) (OYJL_IS_NUMBER(v) && ((v)->u.number.flags & OYJL_NUMBER_INT_VALID))
-#define OYJL_IS_DOUBLE(v) (OYJL_IS_NUMBER(v) && ((v)->u.number.flags & OYJL_NUMBER_DOUBLE_VALID))
-#define OYJL_IS_OBJECT(v) (((v) != NULL) && ((v)->type == oyjl_t_object))
-#define OYJL_IS_ARRAY(v)  (((v) != NULL) && ((v)->type == oyjl_t_array ))
-#define OYJL_IS_TRUE(v)   (((v) != NULL) && ((v)->type == oyjl_t_true  ))
-#define OYJL_IS_FALSE(v)  (((v) != NULL) && ((v)->type == oyjl_t_false ))
-#define OYJL_IS_NULL(v)   (((v) != NULL) && ((v)->type == oyjl_t_null  ))
-
-/** Given a oyjl_val_string return a ptr to the bare string it contains,
- *  or NULL if the value is not a string. */
-#define OYJL_GET_STRING(v) (OYJL_IS_STRING(v) ? (v)->u.string : NULL)
-
-/** Get the string representation of a number.  You should check type first,
- *  perhaps using OYJL_IS_NUMBER */
-#define OYJL_GET_NUMBER(v) ((v)->u.number.r)
-
-/** Get the double representation of a number.  You should check type first,
- *  perhaps using OYJL_IS_DOUBLE */
-#define OYJL_GET_DOUBLE(v) ((v)->u.number.d)
-
-/** Get the 64bit (long long) integer representation of a number.  You should
- *  check type first, perhaps using OYJL_IS_INTEGER */
-#define OYJL_GET_INTEGER(v) ((v)->u.number.i)
-
-/** Get a pointer to a oyjl_val_object or NULL if the value is not an object. */
-#define OYJL_GET_OBJECT(v) (OYJL_IS_OBJECT(v) ? &(v)->u.object : NULL)
-
-/** Get a pointer to a oyjl_val_array or NULL if the value is not an object. */
-#define OYJL_GET_ARRAY(v)  (OYJL_IS_ARRAY(v)  ? &(v)->u.array  : NULL)
-
-void       oyjl_tree_to_json         ( oyjl_val            v,
+void       oyjl_tree_to_json         ( yajl_val            v,
                                        int               * level,
                                        char             ** json );
-char *     oyjl_value_text           ( oyjl_val            v,
+char *     oyjl_value_text           ( yajl_val            v,
                                        void*             (*alloc)(size_t size));
-oyjl_val   oyjl_tree_get_value       ( oyjl_val            v,
+yajl_val   oyjl_tree_get_value       ( yajl_val            v,
                                        const char        * xpath );
-oyjl_val   oyjl_tree_get_valuef      ( oyjl_val            v,
+yajl_val   oyjl_tree_get_valuef      ( yajl_val            v,
                                        const char        * format,
                                                            ... );
-int            oyjl_value_count      ( oyjl_val            v );
-oyjl_val       oyjl_value_pos_get    ( oyjl_val            v,
+int            oyjl_value_count      ( yajl_val            v );
+yajl_val       oyjl_value_pos_get    ( yajl_val            v,
                                        int                 pos );
 
 typedef enum {
